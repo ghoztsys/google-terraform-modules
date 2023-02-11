4714db277e5ae2573e00dc8146d2ee07fd908b82
Andrew Wei <andrew@ghozt.sh>
2023-02-10 21:02:29 -0800

    Update outputs of gke_cluster

--- gke_cluster/outputs.tf
+++ gke_cluster/outputs.tf
@@ -1,3 +1,27 @@
+output "client_certificate" {
+  description = "Base64 encoded public certificate used by clients to authenticate to the cluster endpoint."
+  sensitive   = true
+  value       = google_container_cluster.default.master_auth[0].client_certificate
+}
+
+output "client_key" {
+  description = "Base64 encoded private key used by clients to authenticate to the cluster endpoint."
+  sensitive   = true
+  value       = google_container_cluster.default.master_auth[0].client_key
+}
+
+output "cluster_ca_certificate" {
+  description = "Base64 encoded public certificate that is the root of trust for the cluster."
+  sensitive   = true
+  value       = google_container_cluster.default.master_auth[0].cluster_ca_certificate
+}
+
+output "endpoint" {
+  description = "The endpoint of this cluster's Kubernetes master."
+  sensitive   = true
+  value       = google_container_cluster.default.endpoint
+}
+
 output "managed_instance_group_urls" {
   description = "List of instance group URLs assigned to the node pool."
   value       = google_container_node_pool.default.managed_instance_group_urls
