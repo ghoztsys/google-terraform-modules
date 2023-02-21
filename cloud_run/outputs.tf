694e4315e7a1221f4c2bf7eab6eb528799edaf3a
Andrew Wei <andrew@ghozt.sh>
2023-02-21 11:23:10 -0800

    Export service account in cloud_run

--- cloud_run/outputs.tf
+++ cloud_run/outputs.tf
@@ -1,3 +1,8 @@
+output "service_account" {
+  description = "The service account email associated with the Cloud Run service."
+  value       = google_cloud_run_service.default.template[0].spec[0].service_account_name
+}
+
 output "public_url" {
   description = "Public IP of the Cloud Run service."
   value       = google_cloud_run_service.default.status[0].url
