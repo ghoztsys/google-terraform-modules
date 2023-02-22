22277f01aa829ea781f8e615855be8125c2a8682
Andrew Wei <andrew@ghozt.sh>
2023-02-22 09:56:36 -0800

    Update folder module

--- folder/outputs.tf
+++ folder/outputs.tf
@@ -1,19 +1,19 @@
+output "display_name" {
+  description = "The folder's display name."
+  value       = google_folder.default.display_name
+}
+
 output "id" {
   description = "Folder ID."
   value       = google_folder.default.folder_id
 }
 
 output "name" {
-  description = "The folder's display name."
-  value       = google_folder.default.display_name
+  description = "The name of the folder, format: `folders/{folder_id}`."
+  value       = google_folder.default.name
 }
 
 output "parent" {
   description = "The resource name of the parent folder or organization, in the form of `folders/{folder_id}` or `organizations/{org_id}`."
   value       = google_folder.default.parent
-}
-
-output "resource_id" {
-  description = "The resource name of the folder, format: `folders/{folder_id}`."
-  value       = google_folder.default.name
 }
